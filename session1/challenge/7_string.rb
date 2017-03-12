784c2cfb151d18c6a0b899dd772bebe1e681c2ce
Rob Adams <robert.andrew.adams@gmail.com>
2017-03-12 12:10:37 +0000

    Passing 1:7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  output = ''
+  string.each_char.with_index do |letter, i|
+    unless string[i + 1].nil?
+      output += string[i + 1] if letter == 'r' || letter == 'R'
+    end
+  end
+  output
 end
